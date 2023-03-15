1bb67ef48200c55d1f054c5fbd6ff527ffd33e7b
Stefan <p.c.s.vermeent@uu.nl>
2023-03-15 10:37:47 +0100

    working version of removal task

--- 2023_sem1_WM/materials/removal/1_removal_eco.js
+++ 2023_sem1_WM/materials/removal/1_removal_eco.js
@@ -142,6 +142,179 @@ var stimuli_round5_eco = [
 {stim: 'removal/stimuli/BOSS/avcable.png', version: 'eco', round: '5', probetype: 'REMEMBER'},
 ]
 
+var removal_eco_recall_round0 = {
+  type: jsPsychSurveyMultiSelect,
+  questions: [
+    {
+      prompt: "Which images did you have to remember?", 
+      options: ['<img src="removal/stimuli/practice/apple03a.png" width="70"></img>',
+'<img src="removal/stimuli/practice/breadslice.png" width="70"></img>',
+'<img src="removal/stimuli/practice/cabbage.png" width="70"></img>',
+'<img src="removal/stimuli/practice/cherrytomato02.png" width="70"></img>',
+'<img src="removal/stimuli/practice/banana04b.png" width="70"></img>',
+'<img src="removal/stimuli/practice/broccoli01b.png" width="70"></img>',
+'<img src="removal/stimuli/practice/cauliflower02.png" width="70"></img>',
+'<img src="removal/stimuli/practice/cookie02a.png" width="70"></img>',
+],
+      horizontal: false,
+      required: false,
+      name: 'Recall'
+    }, 
+  ], 
+  data: {
+    variable: 'recall',
+    task: 'removal',
+    round: '0'
+  }
+};
+
+var removal_eco_recall_round1 = {
+  type: jsPsychSurveyMultiSelect,
+  questions: [
+    {
+      prompt: "Which images did you have to remember?", 
+      options: [
+        '<img src="removal/stimuli/BOSS/threeholepunch03.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/pencilsharpener02a.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/spatula03.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/qtip.png" width="70"></img>', 
+'<img src="removal/stimuli/BOSS/manshoe.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/tuque03a.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/makeupbrush04.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/spoon01.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/grater01a.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/overalls.png" width="70"></img>'
+],
+      horizontal: false,
+      required: false,
+      name: 'Recall'
+    }, 
+  ], 
+  data: {
+    variable: 'recall',
+    task: 'removal',
+    round: '1'
+  }
+};
+
+var removal_eco_recall_round2 = {
+  type: jsPsychSurveyMultiSelect,
+  questions: [
+    {
+      prompt: "Which images did you have to remember?", 
+      options: [
+        '<img src="removal/stimuli/BOSS/sock01a.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/plate01b.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/measuringcup01.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/toaster01.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/highheelshoe01.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/shirt01.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/moccasin.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/remotecontrol04.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/alarmclock01.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/soapdispenser01.png" width="70"></img>',
+],
+      horizontal: false,
+      required: false,
+      name: 'Recall'
+    }, 
+  ], 
+  data: {
+    variable: 'recall',
+    task: 'removal',
+    round: '2'
+  }
+};
+
+var removal_eco_recall_round3 = {
+  type: jsPsychSurveyMultiSelect,
+  questions: [
+    {
+      prompt: "Which images did you have to remember?", 
+      options: [
+        '<img src="removal/stimuli/BOSS/pencil01.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/computermouse06.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/mechanicalpencil02.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/shorts01.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/slipper01b.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/cd.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/tupperware03a.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/laptop01a.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/foodprocessor.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/cap01a.png" width="70"></img>',
+],
+      horizontal: false,
+      required: false,
+      name: 'Recall'
+    }, 
+  ], 
+  data: {
+    variable: 'recall',
+    task: 'removal',
+    round: '3'
+  }
+};
+
+var removal_eco_recall_round4 = {
+  type: jsPsychSurveyMultiSelect,
+  questions: [
+    {
+      prompt: "Which images did you have to remember?", 
+      options: [
+        '<img src="removal/stimuli/BOSS/ruler04.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/highlighter02b.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/ramekin01.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/coffeepot03a.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/fryingpan02a.png" width="70"></img>', 
+'<img src="removal/stimuli/BOSS/garlicpress02a.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/pitcher02b.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/bleachbottle.png" width="70"></img>', 
+'<img src="removal/stimuli/BOSS/strainer02.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/bowl01.png" width="70"></img>',
+],
+      horizontal: false,
+      required: false,
+      name: 'Recall'
+    }, 
+  ], 
+  data: {
+    variable: 'recall',
+    task: 'removal',
+    round: '4'
+  }
+};
+
+var removal_eco_recall_round5 = {
+  type: jsPsychSurveyMultiSelect,
+  questions: [
+    {
+      prompt: "Which images did you have to remember?", 
+      options: [
+        '<img src="removal/stimuli/BOSS/corkscrew03a.png" width="70"></img>', 
+'<img src="removal/stimuli/BOSS/measuringspoon.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/envelope03a.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/styrofoamcup.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/cottonpad.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/sandal.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/mug01.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/eraser.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/spatula04.png" width="70"></img>',
+'<img src="removal/stimuli/BOSS/avcable.png" width="70"></img>',
+],
+      horizontal: false,
+      required: false,
+      name: 'Recall'
+    }, 
+  ], 
+  data: {
+    variable: 'recall',
+    task: 'removal',
+    round: '5'
+  }
+};
+
+
+
 var removal_round1_start = {
   type: jsPsychHtmlKeyboardResponse,
   stimulus:    "<p style = 'text-align: center;'>" +
