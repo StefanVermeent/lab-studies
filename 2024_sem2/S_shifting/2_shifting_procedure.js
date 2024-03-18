8e3a2c00428e8ba7536a805b58c813c8af680e45
Stefan <p.c.s.vermeent@gmail.com>
2024-03-18 13:55:03 +0100

    change u to je

--- 2024_sem2/S_shifting/2_shifting_procedure.js
+++ 2024_sem2/S_shifting/2_shifting_procedure.js
@@ -111,7 +111,7 @@ var shifting_procedure02 = {
 
 var shifting_interblock = {
   type: jsPsychHtmlButtonResponse,
-  stimulus: "Goed gedaan! Je bent nu halverwege.<br>Neem even pauze als u dat nodig heeft en druk op 'verder' als je klaar bent voor de rest van het spel.<br><br>",
+  stimulus: "Goed gedaan! Je bent nu halverwege.<br>Neem even pauze als je dat nodig hebt en druk op 'verder' als je klaar bent voor de rest van het spel.<br><br>",
   choices: ['verder'],
   data: {
     task: 'shifting',
